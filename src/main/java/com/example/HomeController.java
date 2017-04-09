a70e4eaa949b6b722da71af11578c14bcb016445
bollerdominik <boller.dominik@gmail.com>
2017-04-09 07:45:04 -0700

    fixed api

--- src/main/java/com/example/HomeController.java
+++ src/main/java/com/example/HomeController.java
@@ -4,13 +4,112 @@ package com.example;
  * Created by dubsta on 22.02.2017.
  */
 
+import java.util.HashMap;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.example.domain.Employee;
+import com.example.domain.Shift;
+import com.example.domain.respository.EmployeeRepository;
+import com.example.domain.respository.ShiftRepository;
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.adapters.RefreshableKeycloakSecurityContext;
+import org.keycloak.representations.AccessToken;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class HomeController {
-    @RequestMapping(value = "/")
-    public String index() {
-        return "index";
+
+    private final EmployeeRepository repositoryEmp;
+    private final ShiftRepository repositoryShift;
+
+    @Autowired
+    public HomeController(EmployeeRepository repositoryEmp, ShiftRepository repositoryShift) {
+        this.repositoryEmp = repositoryEmp;
+        this.repositoryShift = repositoryShift;
+    }
+
+    // Add a shift to an employee
+    @RequestMapping(value = "/api/employees/{eID}/shifts/{sID}", method = RequestMethod.PUT)
+    public @ResponseBody
+    Shift addShiftToEmployee(@PathVariable("eID") long eid, @PathVariable("sID") long sid) {
+        Shift shift = repositoryShift.findOne(sid);
+        Employee emp = repositoryEmp.findOne(eid);
+
+        if (shift == null || emp == null){
+            return null;
+        }
+
+        List<Shift> listS= emp.getShifts();
+        boolean alreadyExists = false;
+        for (Shift s : listS){
+            if (s.getId() == shift.getId()){
+                alreadyExists = true;
+            }
+        }
+
+        if (!alreadyExists){
+            listS.add(shift);
+        }
+
+        emp.setShifts(listS);
+        this.repositoryEmp.save(emp);
+
+        return shift;
+    }
+
+    @RequestMapping(value="/api/employees",method = RequestMethod.GET)
+    @ResponseBody
+    public List<Employee> getEmployees(KeycloakPrincipal<RefreshableKeycloakSecurityContext> principal) {
+
+        AccessToken token = principal.getKeycloakSecurityContext().getToken();
+
+        String id = token.getId();
+        String firstName = token.getGivenName();
+        String lastName = token.getFamilyName();
+        return (List<Employee>) repositoryEmp.findAll();
+    }
+
+    @RequestMapping(value="/api/employee/save",method = RequestMethod.POST)
+    @ResponseBody
+    public HashMap<String, Object> handleEmployee(Employee emp, HttpServletRequest req) {
+
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        if(emp.getId() != null)
+            map.put("message", "Updated employee successfully");
+        else
+            map.put("message", "Added employee successfully");
+        System.out.println(emp.getFirstName());
+        System.out.println(req.getParameter("firstName"));
+
+        emp = repositoryEmp.save(emp);
+
+        map.put("emp", emp);
+        return map;
+    }
+
+    @RequestMapping(value="/api/employee/get/{id}",method = RequestMethod.GET)
+    @ResponseBody
+    public Employee handleEmployee(@PathVariable("id") long id) {
+
+        return repositoryEmp.findOne(id);
+    }
+
+    @RequestMapping(value="/api/employee/delete/{id}",method = RequestMethod.GET)
+    @ResponseBody
+    public Object deleteEmployee(@PathVariable("id") long id) {
+
+        repositoryEmp.delete(id);
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        map.put("message", "Delete employee Successfully");
+        return map;
     }
 }
+
--- src/main/java/com/example/domain/Employee.java
+++ src/main/java/com/example/domain/Employee.java
@@ -1,9 +1,10 @@
 package com.example.domain;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.Data;
 import lombok.Setter;
-
 import javax.persistence.*;
+import java.io.Serializable;
 import java.util.List;
 
 /**
@@ -11,7 +12,10 @@ import java.util.List;
  */
 @Data
 @Entity
-public class Employee {
+public class Employee implements Serializable {
+
+
+    private static final long serialVersionUID = 1L;
     private @Id @GeneratedValue Long id;
     private String firstName;
     private String lastName;
@@ -20,7 +24,8 @@ public class Employee {
 
     @ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(name = "employee_shift", joinColumns = @JoinColumn(name = "employee_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "shift_id", referencedColumnName = "id"))
-    @Setter private List<Shift> shifts;
+    @Setter @JsonManagedReference
+    private List<Shift> shifts;
 
     private Employee() {}
 
@@ -30,5 +35,45 @@ public class Employee {
         this.day = day;
         this.shifts = shifts;
 
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Days getDay() {
+        return day;
+    }
+
+    public void setDay(Days day) {
+        this.day = day;
+    }
+
+    public List<Shift> getShifts() {
+        return shifts;
+    }
+
+    public void setShifts(List<Shift> shifts) {
+        this.shifts = shifts;
     }
 }
